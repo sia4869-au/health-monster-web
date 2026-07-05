d07918aef51231c4cb5bcda93fa5aa9f3d5fad27
sia4869-au <sotakenta2000.2005@gmail.com>
2026-07-05 20:43:04 +0900

    update

--- src/utils/monsterAssets.ts
+++ src/utils/monsterAssets.ts
@@ -2,13 +2,13 @@
 export const monsterAssetMap = {
   DarkletHome: require("../../assets/darklet_home.png"),
   ShadowlHome: require("../../assets/shadowl_home.png"),
-  NightVelosHome: require("../../assets/dark_velos_home.png"),
+  NightVelosHome: require("../../assets/night_velos_home.png"),
   ChaosValdHome: require("../../assets/chaos_vald_home.png"),
   FinalDarkValdHome: require("../../assets/final_darkvald_home.png"),
 
   Darklet: require("../../assets/darklet.png"),
   Shadowl: require("../../assets/shadowl.png"),
-  NightVelos: require("../../assets/dark_velos.png"),
+  NightVelos: require("../../assets/night_velos.png"),
   ChaosVald: require("../../assets/chaos_vald.png"),
   FinalDarkVald: require("../../assets/final_darkvald.png"),
 };
